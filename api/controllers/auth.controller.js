869b24f6739fcc47625089369d0406a6dbdca9e2
blinklay <danillitvinenkod00@gmail.com>
2025-07-30 20:15:23 +0300

    Create signin API route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const userModel = require("../models/user.model.js")
 const bcrypt = require("bcrypt")
 const errorHandler = require("../utils/error.js")
+const jwt = require("jsonwebtoken")
 const signup = async (req, res, next) => {
   const { username, email, password } = req.body
 
@@ -27,4 +28,32 @@ const signup = async (req, res, next) => {
   }
 }
 
-module.exports = { signup }
+const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400, "Все поля должны быть заполнены!"))
+  }
+
+  try {
+    const validUser = await userModel.findOne({ email })
+    if (!validUser) {
+      return next(errorHandler(404, "Пользоваетль не найден!"))
+    }
+
+    const validPassword = bcrypt.compareSync(password, validUser.password)
+    if (!validPassword) {
+      return next(errorHandler(404, "Неверный пароль!"))
+    }
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET_KEY)
+    const { password: pass, ...rest } = validUser._doc
+    res.status(200).cookie("access_token", token, {
+      httpOnly: true,
+    }).json(rest)
+  } catch (error) {
+    next(error)
+  }
+}
+
+module.exports = { signup, signin }
