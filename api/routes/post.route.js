1361ed58afc85c13ade8f9b2b16290466933e472
blinklay <danillitvinenkod00@gmail.com>
2025-08-10 18:58:09 +0300

    Create get posts API route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const verifyUser = require("../middlewares/verifyUser");
-const { create } = require("../controllers/post.controller");
+const { create, getPosts } = require("../controllers/post.controller");
 const postRouter = express.Router()
 
 postRouter.post("/create", verifyUser, create)
+postRouter.get("/getposts", verifyUser, getPosts)
 
 module.exports = postRouter;
 
